0f932d3d67a8d5321ecc15d6644c4ece1ea2aa5d
Kyle Mitchell <45919695+jcmkk3@users.noreply.github.com>
2022-08-23 08:50:41 -0700

    Most basic implementation

---
+++ code.py
@@ -0,0 +1,41 @@
+import board
+import keypad
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+
+ROWS = (board.D0, board.D1, board.D2, board.D3, board.D4, board.D5)
+COLS = (board.D6, board.D7, board.D8, board.D9, board.D10)
+TRANSFORM = (
+    (0,0), (1,0), (0,1), (1,1), (0,2), (1,2), (0,3), (1,3), (0,4), (1,4),
+    (2,0), (3,0), (2,1), (3,1), (2,2), (3,2), (2,3), (3,3), (2,4), (3,4),
+           (4,0), (5,0), (4,1),               (5,2), (4,3), (5,3),
+                          (5,1), (4,2),       (5,4), (4,4)
+)
+
+KEYMAP = (
+    Keycode.W, Keycode.F, Keycode.M, Keycode.P, Keycode.G, Keycode.K, Keycode.U, Keycode.O, Keycode.Y, Keycode.QUOTE,
+    Keycode.R, Keycode.S, Keycode.N, Keycode.T, Keycode.B, Keycode.J, Keycode.A, Keycode.E, Keycode.I, Keycode.H,
+               Keycode.C, Keycode.L, Keycode.D,                       Keycode.X, Keycode.COMMA, Keycode.PERIOD,
+                          Keycode.FORWARD_SLASH, Keycode.SPACE, Keycode.SHIFT, Keycode.SEMICOLON
+)
+
+def key_position(row, col, n_cols):
+    return row * n_cols + col
+
+translate = {
+    key_position(row, col, n_cols=len(COLS)): i
+    for i, (row, col) in enumerate(TRANSFORM)
+}
+
+keys = keypad.KeyMatrix(ROWS, COLS, columns_to_anodes=False)
+kbd = Keyboard(usb_hid.devices)
+
+while True:
+    event = keys.events.get()
+    if event:
+        key_number = event.key_number
+        if event.pressed:
+            kbd.press(KEYMAP[translate[key_number]])
+        if event.released:
+            kbd.release(KEYMAP[translate[key_number]])
